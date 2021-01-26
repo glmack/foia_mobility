81445dfbb4a0e024fb98648d1a6ad8ff5fd77045
glmack <glmack@gmail.com>
2021-01-25 21:54:44 -0800

    Add update

--- pdf_conv.py
+++ pdf_conv.py
@@ -5,10 +5,16 @@ import pytesseract
 import tabula
 
 filepath = '/Users/lee/Documents/code/foia_mobility/TRip_Relocation.pdf'
-doc = convert_from_path(filepath)
-path, filename = os.path.split(filepath)
-filebasename, filextension = os.path.splitext(filename)
+# doc = convert_from_path(filepath)
+# path, filename = os.path.split(filepath)
+# filebasename, filextension = os.path.splitext(filename)
 
-for page_number, page_data in enumerate(doc):
-    txt = pytesseract.image_to_string(Image.fromarray(page_data)) #.encode("utf-8")
-    print("Page # {} - {}".format(str(page_number),txt))
+# for page_number, page_data in enumerate(doc):
+#     txt = pytesseract.image_to_string(Image.fromarray(page_data)) #.encode("utf-8")
+#     print("Page # {} - {}".format(str(page_number),txt))
+
+#--------
+# response = requests.get()
+
+dfs = tabula.read_pdf(filepath, multiple_tables=True, pages='all') # stream=True
+dfs2 = tabula.read_pdf(filepath, multiple_tables=True, pages='all', stream='True')
