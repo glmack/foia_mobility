1e2c3530a6429e2015c4b91c366c6befb9986b15
glmack <glmack@gmail.com>
2021-01-26 22:32:24 -0800

    Iterate on TRIP tables

--- pdf_conv.py
+++ pdf_conv.py
@@ -3,6 +3,7 @@ from PIL import Image
 from pdf2image import convert_from_path
 import pytesseract
 import tabula
+import requests
 
 filepath = '/Users/lee/Documents/code/foia_mobility/TRip_Relocation.pdf'
 # doc = convert_from_path(filepath)
@@ -15,7 +16,10 @@ filepath = '/Users/lee/Documents/code/foia_mobility/TRip_Relocation.pdf'
 
 #--------
 # response = requests.get()
+response = requests.get('https://d2d.gsa.gov/report/gsa-ogp-business-travel-and-relocation-dashboard')
+data = response.json
 
 dfs = tabula.read_pdf(filepath, multiple_tables=True, pages='all') # stream=True
 dfs2 = tabula.read_pdf(filepath, multiple_tables=True, pages='all', stream='True')
 x = dfs[16].T
+dfs3 = tabula.read_pdf('https://d2d.gsa.gov/report/gsa-ogp-business-travel-and-relocation-dashboard')
