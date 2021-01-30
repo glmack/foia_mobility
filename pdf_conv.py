be6307ecfa7f00dd088b6d0e6e255099579b8e94
glmack <glmack@gmail.com>
2021-01-29 19:32:09 -0800

    Iterate on trip data and regs call

--- pdf_conv.py
+++ pdf_conv.py
@@ -1,5 +1,6 @@
 import tabula
 import requests
+import matplotlib.pyplot as plt
 import pandas as pd
 
 filepath = '/Users/lee/Documents/code/foia_mobility/TRip_Relocation.pdf'
@@ -7,7 +8,9 @@ filepath = '/Users/lee/Documents/code/foia_mobility/TRip_Relocation.pdf'
 response = requests.get('https://d2d.gsa.gov/report/gsa-ogp-business-travel-and-relocation-dashboard')
 
 dfs = tabula.read_pdf(filepath, multiple_tables=True, pages=8)
-total_2019 = dfs[1]
+df_total_2019 = dfs[1]
+
+
 fig, ax = plt.subplots()
 fig.patch.set_visible(False)
 ax.axis('off')
@@ -16,5 +19,7 @@ ax.table(cellText=total_2019.values, colLabels=total_2019.columns, loc='center')
 fig.tight_layout()
 plt.show()
 
-# make api call to us gov gsa regulations api
-response = requests.get('https://api.regulations.gov/v4/comments?filter[searchTerm]=travel&api_key=DEMO_KEY')
+# make call to us gov gsa regulations api
+# response = requests.get('https://api.regulations.gov/v4/comments?filter[searchTerm]=travel&api_key=DEMO_KEY',
+# params={'filter[searchTerm]': '115–34'},
+# )
