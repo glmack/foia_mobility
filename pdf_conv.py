d853ca8710e45ea5f3b4fbd1ebbc3c63b7020c45
glmack <glmack@gmail.com>
2021-02-04 09:29:54 -0800

    Roll iterations up into wip functions

--- pdf_conv.py
+++ pdf_conv.py
@@ -3,36 +3,52 @@ import requests
 import matplotlib.pyplot as plt
 import pandas as pd
 
-filepath = '/Users/lee/Documents/code/foia_mobility/TRip_Relocation.pdf'
 
-response = requests.get('https://d2d.gsa.gov/report/gsa-ogp-business-travel-and-relocation-dashboard')
-
-dfs = tabula.read_pdf(filepath, multiple_tables=True, pages=8)
-df_total_2019 = dfs[1]
-
-
-fig, ax = plt.subplots()
-fig.patch.set_visible(False)
-ax.axis('off')
-ax.axis('tight')
-ax.table(cellText=total_2019.values, colLabels=total_2019.columns, loc='center')
-fig.tight_layout()
-plt.show()
-
-# make call to us gov gsa regulations api
-# 'https://api.regulations.gov/v4/documents?3&api_key=DEMO_KEY',
-# response = requests.get(https://api.regulations.gov/v4/documents,
-# params={'filter[searchTerm]': '115–34'},
-# )
-
-def get regulation:
-    response = requests.get(
+def get_trip_report():
+    """Get 2/2020 trip report from d2d dashboard"""
+    filepath = '/Users/lee/Documents/code/foia_mobility/TRip_Relocation.pdf'
+    response = requests.get('https://d2d.gsa.gov/report/gsa-ogp-business-travel-and-relocation-dashboard')
+    dfs = tabula.read_pdf(filepath, multiple_tables=True, pages=8)
+    df_total_2019 = dfs[1]
+    return df_total_2019
 
 
-def get datgov_metadata():
+def vis_trip_report():
+    """Visualize trip report data"""
+    fig, ax = plt.subplots()
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    ax.axis('tight')
+    ax.table(cellText=total_2019.values, colLabels=total_2019.columns, loc='center')
+    fig.tight_layout()
+    plt.show()
+
+
+def get_datagov_meta():
+    """Request metadata on US gov datasets from data.gov"""
     import requests
-    payload = {'id': 'concur-travel-parent', 'api_key': os.environ['DATAGOV_API_KEY']}
-    response = requests.get('https://api.gsa.gov/technology/datagov/v3/action/package_show',
-                            params=payload
-                            )
-    return response.json()
+    payload = {'id': 'concur-travel-parent',
+               'api_key': os.environ['DATAGOV_API_KEY']}
+    response = requests.get(
+        'https://api.gsa.gov/technology/datagov/v3/action/package_show',
+        params=payload
+        )
+    data = response.json()
+    return data
+
+
+def get_regulation():
+    """Make call to us gov gsa regulations api"""
+    payload = {'filter[searchTerm]': '115–34',
+               'api_key': os.environ['DATAGOV_API_KEY']}
+    response = requests.get(
+        'https://api.regulations.gov/v4/documents?3&api_key=DEMO_KEY',
+        params=payload
+        )
+    data = response.json()
+    return data
+
+
+# ----------
+
+concur_travel_parent_meta = get_datagov_meta()
