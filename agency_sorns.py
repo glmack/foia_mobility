3792486675054022f586a8eb945ddc7d0e084e91
glmack <glmack@gmail.com>
2021-07-13 10:29:58 -0700

    Iterate

--- agency_sorns.py
+++ agency_sorns.py
@@ -3,6 +3,8 @@ import requests
 import pandas as pd
 import re
 
+# [dos, gsa, ded, dod, ]
+
 def get_dos_sorns():
     """Get list of Department of State record systems from website"""
     import requests
@@ -43,38 +45,57 @@ def get_dos_sorns():
     return datasets
 
 
+def get_gsa_sorns():
+    """Get list of ___ record systems from website"""
+    import requests
+    url = 'https://www.gsa.gov/reference/gsa-privacy-program/systems-of-records-privacy-act/system-of-records-notices-sorns-privacy-act'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    body = soup.find('tbody')
+    dataset_els = body.find_all('tr')
+    datasets = []
+    dataset_row = {}
+
+
 def get_ded_sorns():
     """Get list of Department of Education record systems from website"""
+
 
 def get_dod_sorns():
     """Get list of Department of Education record systems from website"""
 
-def get_   _sorns():
-    """Get list of ___ record systems from website"""
 
 def get_   _sorns():
     """Get list of ___ record systems from website"""
 
-def get_   _sorns():
-    """Get list of ___ record systems from website"""
 
 def get_   _sorns():
     """Get list of ___ record systems from website"""
 
-def get_   _sorns():
-    """Get list of ___ record systems from website"""
 
 def get_   _sorns():
     """Get list of ___ record systems from website"""
 
-def get_   _sorns():
-    """Get list of ___ record systems from website"""
 
 def get_   _sorns():
     """Get list of ___ record systems from website"""
 
+
 def get_   _sorns():
     """Get list of ___ record systems from website"""
+
+
+def get_   _sorns():
+    """Get list of ___ record systems from website"""
+
+
+def get_   _sorns():
+    """Get list of ___ record systems from website"""
+
+
+def get_   _sorns():
+    """Get list of ___ record systems from website"""
+
 
 def get_   _sorns():
     """Get list of ___ record systems from website"""
