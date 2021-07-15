16b8fbe2d05a7bd8b8ac8bd915d0f5b4532a621d
glmack <glmack@gmail.com>
2021-07-15 10:30:42 -0700

    Iterate on treasury and var renaming in all agency funcs

--- agency_sorns.py
+++ agency_sorns.py
@@ -11,19 +11,20 @@ def get_dos_sorns():
     url = 'https://www.state.gov/system-of-records-notices-privacy-office/'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    body = soup.find('tbody')
-    dataset_els = body.find_all('tr')
     datasets = []
-    dataset_row = {}
-    for dataset_idx, dataset_el in enumerate(dataset_els):
-        if dataset_idx == 0:
+    table_row = {}
+
+    body = soup.find('tbody')
+    table_row_els = body.find_all('tr')
+    for table_row_idx, table_row_el in enumerate(table_row_els):
+        if table_row_idx == 0:
             pass
-        elif dataset_idx != 0:
-            column_els = dataset_el.find_all('td')
-            for col_idx, col_el in enumerate(column_els):
-                if col_idx == 0:
+        elif table_row_idx != 0:
+            table_data_els = table_row_el.find_all('td')
+            for table_data_idx, table_data_el in enumerate(table_data_els):
+                if table_data_idx == 0:
                     # TODO - Lee: split rec_sys_name from recsys_code
-                    name_and_code = col_el.text.split(',')
+                    name_and_code = table_data_el.text.split(',')
                     try:
                         name = name_and_code[0].strip()
                     except:
@@ -32,23 +33,78 @@ def get_dos_sorns():
                         name_code = name_and_code[1].strip()
                     except:
                         name_code = ''
-                    dataset_row = {'name': name,
+                    table_row = {'name': name,
                                    'name_code': name_code
                                       }
                 elif col_idx == 1:
                     pass
                 elif col_idx == 2:
-                    dataset_row['sorn_code'] = col_el.text.strip()
+                    table_row['sorn_code'] = col_el.text.strip()
                     try:
-                        doc_url = col_el.a['href']
-                        dataset_row['doc_url'] = doc_url
+                        doc_url = table_data_string['href']
+                        table_row['doc_url'] = doc_url
                     except:
                         pass
                 else:
                     pass
-            datasets.append(dataset_row)
+            datasets.append(table_row)
         else:
             pass
+    return datasets
+
+
+def get_treasury_sorns():
+    """Get list of record systems of US Department of Treasury"""
+    url = 'https://home.treasury.gov/footer/privacy-act/system-of-records-notices-sorns'
+    import requests
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    body = soup.find('section')
+    table_els = body.find_all('table')
+    datasets = [] # TODO Lee check indent?
+
+
+    for table_el in table_els:
+        dataset_row = {} # TODO Lee check indent
+        dataset_els = body.find_all('tr')
+        table_el.text
+
+        for dataset_idx, dataset_el in enumerate(dataset_els):
+            if dataset_idx == 0:
+                pass
+            elif dataset_idx != 0:
+                table_data_els = dataset_el.find_all('td')
+                for table_data_idx, table_data_el in enumerate(table_data_els):
+                    table_data_string = table_data_el.a.text.split('-')
+                    if table_data_idx == 0:
+                        # TODO - Lee: split rec_sys_name from recsys_code
+                        try:
+                            name_code = table_data_string[0].strip()
+                        except:
+                            name_code = ''
+                            pass
+                        dataset_row = {'name_code': name_code
+                                      }
+                    elif col_idx == 1:
+                        try:
+                            name = table_data_string[1].strip()
+                        except:
+                            name = ''
+                            pass
+                        dataset_row['name'] = name
+                    elif col_idx == 2:
+                        dataset_row['sorn_code'] = table_data_string[2].strip()
+                        try:
+                            doc_url = col_el.a['href']
+                            dataset_row['doc_url'] = doc_url
+                        except:
+                            doc_url = ''
+                            pass
+                    else:
+                        pass
+                datasets.append(dataset_row)
+            else:
+                pass
     return datasets
 
 
@@ -60,60 +116,6 @@ def get_ded_sorns():
 def get_dod_sorns():
     """Get list of record systems of US Department of Education"""
     url = 'https://dpcld.defense.gov/Privacy/SORNsIndex/'
-
-def get_treasury_sorns():
-    """Get list of record systems of US Department of Treasury"""
-    url = 'https://home.treasury.gov/footer/privacy-act/system-of-records-notices-sorns'
-    import requests
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    body = soup.find('section')
-    table_els = body.find_all('table')
-    for table_el in table_els:
-        if table_el.title.text == pass:
-            pass
-        elif table_el.title.text == pass:
-            pass
-        else:
-            pass
-        dataset_els = body.find_all('tr')
-    datasets = [] # TODO Lee check indent?
-    dataset_row = {} # TODO Lee check indent
-        for dataset_idx, dataset_el in enumerate(dataset_els):
-            if dataset_idx == 0:
-                pass
-            elif dataset_idx != 0:
-                column_els = dataset_el.find_all('td')
-                for col_idx, col_el in enumerate(column_els):
-                    if col_idx == 0:
-                        # TODO - Lee: split rec_sys_name from recsys_code
-                        name_and_code = col_el.text.split(',')
-                        try:
-                            name = name_and_code[0].strip()
-                        except:
-                            name = ''
-                        try:
-                            name_code = name_and_code[1].strip()
-                        except:
-                            name_code = ''
-                        dataset_row = {'name': name,
-                                    'name_code': name_code
-                                        }
-                    elif col_idx == 1:
-                        pass
-                    elif col_idx == 2:
-                        dataset_row['sorn_code'] = col_el.text.strip()
-                        try:
-                            doc_url = col_el.a['href']
-                            dataset_row['doc_url'] = doc_url
-                        except:
-                            pass
-                    else:
-                        pass
-                datasets.append(dataset_row)
-            else:
-                pass
-    return datasets
 
 
 def get_usda_sorns():
